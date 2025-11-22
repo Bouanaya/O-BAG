020878b47a2c85117235dbae737d02cf9099b44f
Bouanaya <bouanayasoufiane@gmail.com>
2025-11-22 14:03:34 +0000

    Add Embla Autoplay Package and Update Home Page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,22 @@
 import { CarouselHome } from "@/components/CarouselHome";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 
 
 const Page = () => {
   return (
-    <div  >
-      <div className="flex justify-center items-center  h-full">
-     
+    <div className="relative" >
+      <div className="absolute z-50 right-0 top-1/2 w-25 h-30 bg-black flex flex-col items-center justify-center rounded-l-lg">
+        <Image src="/sac.gif" alt="Logo" width={50} height={50} />
+      <Link href="/login">
+          <span className="text-white text-sm ">vouz créez</span>
+          
+        </Link>
+      </div>
+      <div className="flex justify-center items-center  h-full ">
+
          <CarouselHome />
       
       </div>
