e9341ca1e53e7a4dee4a4ba140cf6148a96dd468
Bouanaya <bouanayasoufiane@gmail.com>
2025-11-22 23:49:00 +0000

    Refactor home page to use Magento and Poignees
    Replace inline promo with modular components Remove unused Image and
    Link imports Render Magento, CarouselHome, and Poignees on homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,22 +1,13 @@
 import { CarouselHome } from "@/components/CarouselHome";
-import Image from "next/image";
-import Link from "next/link";
-
-
+import Magento from "@/components/Magento";
+import Poignees from "@/components/Poignees";
 
 const Page = () => {
   return (
-    <div className="relative mt-28 " >
-      <div className="absolute z-50 right-0 top-1/2 w-25 h-30 bg-black flex flex-col items-center justify-center rounded-l-lg">
-        <Image src="/sac.gif" alt="Logo" width={50} height={50} />
-      <Link href="/login">
-          <span className="text-white text-sm ">vouz créez</span>
-          
-        </Link>
-      </div>
-      <div className=" ">
-        <CarouselHome />
-      </div>
+    <div className="relative mt-28 ">
+      <Magento />
+      <CarouselHome />
+      <Poignees />
     </div>
   );
 };
