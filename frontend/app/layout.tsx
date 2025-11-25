1d8f01f344f73112bbaa77109ad409071c8c438f
Bouanaya <bouanayasoufiane@gmail.com>
2025-11-25 18:52:06 +0000

    feat(products): add product details page

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/HeroSection/Header";
+import Footer from "@/components/HeroSection/Footer";
 
 
 
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,22 @@
-import { CarouselHome } from "@/components/CarouselHome";
-import CreateHero from "@/components/CreateHero";
-import Magento from "@/components/Magento";
-import MonthlyBags from "@/components/MonthlyBags";
-import Poignees from "@/components/Poignees";
-import ProductList from "@/components/ProductList";
+import { CarouselHome } from "@/components/HeroSection/CarouselHome";
+import CreateHero from "@/components/HeroSection/CreateHero";
+import Magento from "@/components/HeroSection/Magento";
+import MonthlyBags from "@/components/HeroSection/MonthlyBags";
+import Poignees from "@/components/HeroSection/Poignees";
+import ProductList from "@/components/HeroSection/ProductList";
+import NewsletterBags from "@/components/HeroSection/NewsletterSection";
 
 const Page = () => {
   return (
-    <div className="relative mt-28 ">
+    <div className="relative mt-28 bg-white ">
       <Magento />
       <CarouselHome />
       <Poignees />
       <ProductList />
       <CreateHero />
       <MonthlyBags />
+      <NewsletterBags />
+      
     </div>
   );
 };
